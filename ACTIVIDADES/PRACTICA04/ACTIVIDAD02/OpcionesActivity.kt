cfe23b14e00f4f793741521ca5ba968c811f5bf1
Esther Mariana Chunga Pacheco <echungap@ulasalle.edu.pe>
2025-10-03 12:10:13 -0500

    Añadiendo parte B Editor de nota rápida a PRCTICA 04

---
+++ ACTIVIDADES/PRACTICA04/ACTIVIDAD02/OpcionesActivity.kt
@@ -0,0 +1,20 @@
+/*
+Descripción: OpcionesActivity - muestra la nota y permite compartir por correo o editar de nuevo
+Autor: Esther Mariana Chunga Pacheco
+Fecha creación: 12/09/2025
+Fecha última modificación: 12/09/2025
+*/
+
+package com.example.practica4
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class OpcionesActivity : AppCompatActivity() {
+    ...
+}
