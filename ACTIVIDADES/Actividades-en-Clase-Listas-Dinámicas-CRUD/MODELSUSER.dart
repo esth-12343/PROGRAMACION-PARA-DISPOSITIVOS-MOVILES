d104ac9f738d41b75b50a19e4738a3e0d12538c5
esth-12343 <esth-12343@ejemplo.com>
2025-11-20 08:46:07 -0500

    subiendo archivos de codigo de Actividades en Clase: Listas Dinámicas y CRUD Local con Provider en Flutter.

---
+++ ACTIVIDADES/Actividades-en-Clase-Listas-Dinámicas-CRUD/MODELSUSER.dart
@@ -0,0 +1,11 @@
+class User {
+  String nombre;
+  String genero; // 'Masculino' o 'Femenino'
+  bool activo;
+
+  User({
+    required this.nombre,
+    required this.genero,
+    required this.activo,
+  });
+}
